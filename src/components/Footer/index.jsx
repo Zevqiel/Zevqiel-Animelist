599a649d57fb2d36765ff4e770c83e9b7be4ab6a
Zevqiel <tiohelambang2020@gmail.com>
2024-07-07 11:38:18 +0700

    add apaweh

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return(
         <footer className="bg-blue-500 py-6">
         <div className="container mx-auto px-6 text-center text-slate-200">
-          <p>&copy; 2024 Company Name. All rights reserved.</p>
+          <p>&copy; 2024 Muhammad Tio Helambang. Website Studi Kasus Animelist.</p>
         </div>
       </footer>
     )
